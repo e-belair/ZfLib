ca5b1b9580c9e86918aaea3d748a187b0f4b210c
Emmanuel Belair <contact@ebelair.fr>
2013-06-03 01:09:02 +0200

    update module config

--- config/module.config.php
+++ config/module.config.php
@@ -1,4 +1,10 @@
 <?php
 
 return array(
+    'view_helpers' => array(
+        'invokables' => array(
+            'a' => 'ZfLib\View\Helper\A',
+            'img' => 'ZfLib\View\Helper\Img',
+        ),
+    ),
 );
